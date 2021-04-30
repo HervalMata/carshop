b652f55ecad9d1bc170e62b066dd7c5107e073c7
Herval Mata <hervalmataamparo@gmail.com>
2021-04-30 00:19:10 -0300

    feat:art: add confirm
    created component for confirm dialog in react project frontend

--- frontend/src/view/components/Loading.js
+++ frontend/src/view/components/Loading.js
@@ -11,7 +11,7 @@ export default function Loading() {
     return (
         <Modal open={loading.open} onClose={() => dispatch(changeLoading({open: false}))}
         className="d-flex justify-content-center align-items-center h-100">
-            <div className="bg-white d-flex align-items-center rounded-lg p-3 outline-none">
+            <div className="bg-white d-flex align-items-center rounded-lg p-3">
                 <CircularProgress size={20} className="mr-3"/>
                 <Typography variant="subtitle1">{loading.msg}</Typography>
             </div>
